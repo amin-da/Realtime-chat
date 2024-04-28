641497ef9f97948a64a8a499a711bf769dc65196
amin-da <darabniya@yahoo.com>
2024-04-28 14:05:28 +0330

    fix : change middleware config for clerk auth

--- middleware.ts
+++ middleware.ts
@@ -5,5 +5,5 @@ export default authMiddleware({
 });
 
 export const config = {
-  matcher: ["/((?!api|trpc))(_next.*|.+.[w]+$)", "/"],
+  matcher:  ["/((?!.+\\.[\\w]+$|_next).*)","/","/(api|trpc)(.*)"],
 };
